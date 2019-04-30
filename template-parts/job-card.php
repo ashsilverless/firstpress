14297db513e15dd6d6d5c085a19e68a5629dd8a2
Ash - SIlverless <ash@silverless.co.uk>
2019-04-30 09:37:22 +0100

    DEV - add files

---
+++ template-parts/job-card.php
@@ -0,0 +1,19 @@
+<div class="job-card">
+    
+    <div class="content">
+    
+    <p class="salary">£28,000</p>
+    
+    <p class="time"><i class="fas fa-clock"></i> 1hr ago</p>
+    
+    <h5 class="heading heading__xs heading__secondary-color font200 mb0">Permanent</h5>
+    <h2 class="heading heading__sm heading__secondary-color mb0">Cocktail Bartender</h5>
+    <h5 class="heading heading__xs heading__secondary-color font200 mb2">SE London</h5>            
+    
+    <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa.</p>
+    
+    </div>
+    
+    <a href=""><i class="fas fa-user-alt"></i> Apply Now</a>    
+    
+</div>
