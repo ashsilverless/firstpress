9e4a453c51879e21e7320add64cf67395bd6f041
Ash - SIlverless <ash@silverless.co.uk>
2019-05-01 10:17:48 +0100

    DEV - minor adjustment

--- page-templates/home.php
+++ page-templates/home.php
@@ -55,46 +55,13 @@ get_header();?>
             </div><!--col-->
             
             <div class="col-5 offset-1 sticky-sidebar">
-	            
-	            
+
 	            <?php
 		            set_query_var('color', 'brand-secondary'); // Primary or Secondary
 		            get_template_part('template-parts/search-block');
 		        ?>
-                
-<<<<<<< HEAD
-                <div class="sidebar-cta">
-                    
-                    <h2 class="heading heading__lg font400">Find Your Perfect Job In Catering</h2>
-                    
-                    <a href="" class="button button__ghost button__fullwidth mt2">Submit CV</a>
-                    
-                </div>
-                
-                <div class="sidebar">
 
-                <?php for ($x = 0; $x <= 5; $x++) {
-                    get_template_part('template-parts/beer', 'glass');
-                } ?>
-
-                    <h3 class="heading heading__md heading__alt-color mb1">Latest Jobs</h3>
-
-
-                <?php for ($x = 0; $x <= 3; $x++) {
-                    get_template_part('template-parts/job', 'card');
-                } ?>
-                    
-                </div>
-
-                <div class="sidebar-cta text-center">
-                    
-                    <a href="" class="heading__light"><h2 class="heading heading__md font400">Search All Jobs</h2></a>
-                    
-                </div>
-                
-=======
                 <?php get_template_part('template-parts/sidebar');?>
->>>>>>> AshDev
                 
             </div><!--col-->            
 
